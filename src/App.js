ee964cb8888ea2450348e9a8ea551fc78c53cd5a
Meltrust <original.mtapia@outlook.com>
2021-10-23 03:28:04 -0500

    Create layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-
-//     );
-//   }
-// }
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends React.Component {
+  render() {
+    return (
+      <main className="bg-dark vh-100 d-flex flex-column justify-content-center">
+        <div className="container">
+          <div className="row justify-content-center">
+            <section className="col-lg-7">
+              <Calculator />
+            </section>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,1 +1,73 @@
 import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="row row-cols-4">
+        <div className="gray-bg text-end text-white col-12 py-4 fs-4 align-self-center">
+          0
+        </div>
+        <button className="fs-5 py-4 border" type="button">
+          AC
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          +/-
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          %
+        </button>
+        <button className="fs-5 py-4 border btn-warning" type="button">
+          &divide;
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          7
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          8
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          9
+        </button>
+        <button className="fs-5 py-4 border btn-warning" type="button">
+          x
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          4
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          5
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          6
+        </button>
+        <button className="fs-5 py-4 border btn-warning" type="button">
+          -
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          1
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          2
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          3
+        </button>
+        <button className="fs-5 py-4 border btn-warning" type="button">
+          +
+        </button>
+        <button className="col-6 fs-5 py-4 border" type="button">
+          0
+        </button>
+        <button className="fs-5 py-4 border" type="button">
+          .
+        </button>
+        <button className="fs-5 py-4 border btn-warning" type="button">
+          =
+        </button>
+
+      </div>
+
+    );
+  }
+}
