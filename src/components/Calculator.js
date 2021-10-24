a9f2537cfffde6c94ea798d97195af8fe9e93bda
Meltrust <original.mtapia@outlook.com>
2021-10-23 20:53:01 -0500

    Fix linter

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
